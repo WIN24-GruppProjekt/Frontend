c0f99c1acd0382b84f5863aef44f1a2867690481
Fredrik <fredd24hate@gmail.com>
2025-09-17 09:31:12 +0200

    started with the buttons

---
+++ src/components/SignInButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignInButton = () => {
+    return (
+        <button className="btn-box" type="submit">
+            <div className="btn-standard">
+                Sign In
+            </div>
+        </button>
+    )
+}
+
+export default SignInButton
