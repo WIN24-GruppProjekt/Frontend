ebc7551c93d092c32adf5bb0cc11c341a38c178a
Fredrik <fredd24hate@gmail.com>
2025-09-19 15:51:25 +0200

    Added buttons to that is suppose to be on the session card. Also added how the buttons react when pressed.

---
+++ src/components/SessionCardButtons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MoreInfoButton from './MoreInfoButton'
+import BookPassButton from './BookPassButton'
+
+const SessionCardButtons = () => {
+    return (
+        <div className="more-info-book-pass">
+            <MoreInfoButton />
+            <BookPassButton />
+        </div>
+    )
+}
+
+export default SessionCardButtons
