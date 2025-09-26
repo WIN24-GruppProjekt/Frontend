2bde26d96c70408efd97a2c6910b53752628ef05
Augustvilliam <willyp@hotmail.se>
2025-09-26 11:16:58 +0200

    Link to Tearms added

---
+++ src/components/TermsOfUse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TermsOfUse() {
+  return (
+    <div className="TermsOfUseContainer">
+        <a href="https://www.youtube.com/watch?v=CT_cPu74ZnU" target="_blank" className="TearmsOfUse">
+            Som användare av Haninge Core så godkänner du terms and usage
+        </a>    
+    </div>        
+  )
+}
