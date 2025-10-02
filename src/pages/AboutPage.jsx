be6201ebb22cac2f5a22c6b810a7db6b9c17cc6d
Hy <hy@Rakhkmahs-MacBook-Pro.local>
2025-10-02 14:18:21 +0330

    the section to show the instructors added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,6 +7,9 @@ const AboutPage = () => {
       <h1 className='about-header'>Om oss</h1>
       <span className='about-text'>På Core Gym Club brinner vi för hälsa, styrka och gemenskap. Vårt mål är att skapa en miljö där alla – oavsett nivå – känner sig välkomna och inspirerade. Med moderna lokaler, engagerade instruktörer och ett brett utbud av träningspass hjälper vi dig att nå dina mål, bygga nya vanor och ha roligt på vägen.</span>
       <img src={Image} alt="Training session" className='about-image' />
+      <section>
+        <h1>This section is supposed to show the instructors of the Core Gym Club</h1>
+      </section>
     </section>
   )
 }
