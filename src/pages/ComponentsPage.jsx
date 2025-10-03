7fbb8a579286227e7dad59e715b8c1197716e1da
Morgan Johansson <morgan.johansson@me.com>
2025-10-03 10:23:43 +0200

    feat: pass imgUrl from API as photoUrl to GymInstructorCard

--- src/pages/ComponentsPage.jsx
+++ src/pages/ComponentsPage.jsx
@@ -4,6 +4,8 @@ import SignOutButton from '../components/SignOutButton'
 import MobileSignIn from '../components/MobileSignIn'
 import MobileSignOut from '../components/MobileSignOut'
 import SessionCardButtons from '../components/SessionCardButtons'
+import InstructorEmil from '../img/instructor-emil.jpg'
+import InstructorHans from '../img/instructor-hans.jpg'
 
 const ComponentsPage = () => {
   return (
@@ -14,6 +16,8 @@ const ComponentsPage = () => {
       <MobileSignIn />
       <MobileSignOut />
       <SessionCardButtons />
+      <img src={InstructorEmil} alt="Emil" style={{ height: '100px', width: '100px', borderRadius: '50%', objectFit: 'cover' }}/>
+      <img src={InstructorHans} alt="Hans" style={{ height: '100px', width: '100px', borderRadius: '50%', objectFit: 'cover' }}/>
     </div>
   )
 }
